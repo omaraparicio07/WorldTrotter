d85fe897abf09143b6bb9de52f8fd649eadbc877
Omar <omaraparicio07@gmail.com>
2019-11-16 12:09:57 -0600

    Add function delegate textField

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -7,7 +7,7 @@
 //
 import UIKit
 
-class ConversionViewController: UIViewController {
+class ConversionViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var celsiusLabel:UILabel!
     
     @IBOutlet var textField:UILabel!
@@ -65,4 +65,21 @@ class ConversionViewController: UIViewController {
             celsiusLabel.text = "???"
         }
     }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+//        print("Current text: \(textField.text)")
+//        print("Replacement text: \(string)")
+//        return true
+        
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        if existingTextHasDecimalSeparator != nil,
+            replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    
 }
