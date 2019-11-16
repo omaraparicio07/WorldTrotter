25a9217ff8075f8c5a03d644df434ac00f54104e
Omar <omaraparicio07@gmail.com>
2019-11-16 00:24:54 -0600

    Add textfield outlet and connect tapGestureRecognizer from main storyboard

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -9,13 +9,18 @@ import UIKit
 
 class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel:UILabel!
+    @IBOutlet var textField:UILabel!
     
-    @IBAction func FahrenheitFieldEditingChanged(_ textField:UITextField ){
+    @IBAction func fahrenheitFieldEditingChanged(_ textField:UITextField ){
         //celsiusLabel.text = textField.text
         if let text = textField.text, !text.isEmpty {
             celsiusLabel.text = text
         }else {
             celsiusLabel.text = "???"
         }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
     }
 }
