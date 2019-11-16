b28de48a2a69e89bd5b948b73eb5d57acdee0501
Omar <omaraparicio07@gmail.com>
2019-11-16 00:41:40 -0600

    Add logic to convert degrees and method to formatter numbers

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -9,18 +9,60 @@ import UIKit
 
 class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel:UILabel!
+    
     @IBOutlet var textField:UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateCelsiusLabel()
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+    return nf
+    }()
     
     @IBAction func fahrenheitFieldEditingChanged(_ textField:UITextField ){
         //celsiusLabel.text = textField.text
-        if let text = textField.text, !text.isEmpty {
-            celsiusLabel.text = text
-        }else {
-            celsiusLabel.text = "???"
+//        if let text = textField.text, !text.isEmpty {
+//            celsiusLabel.text = text
+//        }else {
+//            celsiusLabel.text = "???"
+//        }
+        
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
         }
     }
     
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
         textField.resignFirstResponder()
+    }
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? { didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+//            celsiusLabel.text = "\(celsiusValue.value)"
+            celsiusLabel.text=numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
     }
 }
