473deaf11e24af28098d2b474080fd0454f6dea9
Omar <omaraparicio07@gmail.com>
2019-11-15 23:28:58 -0600

    Connect celsiusLabel and bind editing changes event to ConvertViewController

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -11,6 +11,11 @@ class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel:UILabel!
     
     @IBAction func FahrenheitFieldEditingChanged(_ textField:UITextField ){
-        celsiusLabel.text = textField.text
+        //celsiusLabel.text = textField.text
+        if let text = textField.text, !text.isEmpty {
+            celsiusLabel.text = text
+        }else {
+            celsiusLabel.text = "???"
+        }
     }
 }
