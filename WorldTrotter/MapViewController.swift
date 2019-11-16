f421f1462cee7c8e6155c2b4ee510252589791bc
Omar <omaraparicio07@gmail.com>
2019-11-16 13:57:56 -0600

    Create map view programmatically and adding constraints in code

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
- 
+    var mapView:MKMapView!
+    
+    override func loadView() {
+        mapView=MKMapView()
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo:view.topAnchor)
+        let leadingContrain = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        let trailingConstrait = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingContrain.isActive = true
+        trailingConstrait.isActive = true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("MapViewController loaded its view")
     }
 }
+
