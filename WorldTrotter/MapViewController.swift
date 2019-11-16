b4789121718cb3ea30b2e5a9d2cefea82abbb0e8
Omar <omaraparicio07@gmail.com>
2019-11-16 13:38:21 -0600

    Create new controller class and assosiated to MapView, print in standart output lazy loading views

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Makingdevs on 16/11/19.
+//  Copyright © 2019 Makingdevs. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+ 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded its view")
+    }
+}
